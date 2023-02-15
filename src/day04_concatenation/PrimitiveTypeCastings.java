35fe000a0322e7df3d1ce74bd61f45863e3eca2c
nurhasss <nurhas14@gmail.com>
2023-02-14 23:48:07 -0500

    commit

--- src/day04_concatenation/PrimitiveTypeCastings.java
+++ src/day04_concatenation/PrimitiveTypeCastings.java
@@ -16,6 +16,33 @@ public class PrimitiveTypeCastings {
         //---explicit casting----
         int x=100;
         byte y = (byte)  x;
+        float z = 20.8F;
+        short q = (short) z; // z= 20.8, when it is casted to short, it will only take 20.
+        System.out.println(q);
+        //double is largest primitive data type
+
+        double n1=2.5;
+        byte n2= (byte) n1;
+        System.out.println(n2); //2
+
+        System.out.println("---------------");
+        int num = 500;
+        byte result = (byte) num; //explicit casting
+        System.out.println(result);
+
+        int r = 50000;
+        short t = (short) r;
+        System.out.println(t);
+
+        double u = 3000.5;
+        int p = (int) u; //3000, explicit casting
+        System.out.println(p);
+
+        int o=100;
+        double d1 = o;
+        System.out.println(d1);
+
+
 
 
 
